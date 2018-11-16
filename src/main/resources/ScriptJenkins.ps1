07af0c8431af1e92b845eb567ad08902ce03b38c
Wiktor <wiktor2810@o2.pl>
2018-11-16 15:23:11 +0100

    added scriptjenkins

---
+++ src/main/resources/ScriptJenkins.ps1
@@ -0,0 +1,5 @@
+﻿gc config.properties | Foreach-Object {
+    $_ -replace "browser", "$env:browser1"
+    } | Set-Content tempConfig
+    rm config.properties
+    rename-item tempConfig config.properties
